42dec27971c2f1ae6e9e0c9ad226b7b582dbfd88
YSK <yousafsadiq445@gmail.com>
2023-11-21 16:10:59 +0500

    21-NOV-2023

---
+++ PHP OOPS/project/App/database.php
@@ -0,0 +1,324 @@
+<?php
+
+namespace App\database;
+
+
+class Database
+{
+  // encapsulation rules
+  private $db = "auth";
+  private $username = "root";
+  private $password = "";
+  private $server = "localhost";
+  protected $link; // DATABASE LINKAGE 
+
+  private $query; // MYSQL query 
+  private $exe; // Query EXECUTION SAVE 
+  private $result; // FETCH ASSOC 
+
+
+
+  public function __construct()
+  {
+    $this->link = new \mysqli($this->server, $this->username, $this->password, $this->db);
+
+    if ($this->link) {
+      // echo "Established";
+    }
+  }
+
+
+
+
+
+  // method 1 sample 
+// public function insert($table,$col, $val ){
+
+  // // INSERT INTO table_name (columns) VALUES (values)
+// }
+
+  /* 
+  $table is used for table name
+  $param is used for saving Associative array
+  */
+  public function insert($table, $param)
+  {
+
+    //  implode is used for converting array to string while explode is used for converting string to array
+    $column = implode("`,`", array_keys($param));
+
+    $values = implode("','", array_values($param));
+    // echo $column."<br>";
+// echo $values."<br>";
+
+    if ($this->checkTable($table)) {
+
+      $this->query = " INSERT INTO `{$table}` (`{$column}`) 
+  VALUES ('{$values}')";
+
+      $this->execute($this->query, "DATA HAS BEEN INSERTED");
+
+
+    } else {
+      $abc = new helper();
+
+      $abc->ErrorMSG("TABLE IS NOT EXIST");
+
+    }
+
+    //   $this->query = " INSERT INTO `{$table}` `{$column}` 
+// VALUES '{$values}'";
+
+    // INSERT INTO table_name (columns) VALUES (values)
+
+  }
+
+
+  private function checkTable($table)
+  {
+    $this->query = "SELECT * 
+FROM information_schema.tables
+WHERE table_schema = '{$this->db}'
+    AND table_name = '{$table}'
+LIMIT 1;";
+
+    $this->exe = $this->link->query($this->query);
+
+    if ($this->exe) {
+
+      if ($this->exe->num_rows == 1) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+  }
+
+
+  private function execute($query, $msg = null)
+  {
+
+    //  $query."from execute function";
+    $this->exe = $this->link->query($query);
+    //  Association 
+    $helper = new helper();
+
+    if ($this->exe) {
+
+      if ($msg != null) {
+
+        $helper->SuccessMSG($msg);
+
+      } else {
+        return true;
+      }
+
+
+
+    } else {
+
+      $helper->ErrorMSG("ERROR in QUERY: " . $this->query);
+    }
+
+  }
+
+
+  public function GetRESULT()
+  {
+    $this->result = $this->exe->fetch_assoc();
+    return $this->result;
+  }
+
+  public function getSql()
+  {
+    return $this->query;
+  }
+
+  public function getPrimaryKey($table)
+  {
+
+    $query = "SELECT k.COLUMN_NAME
+  FROM information_schema.table_constraints t
+  LEFT JOIN information_schema.key_column_usage k
+  USING(constraint_name,table_schema,table_name)
+  WHERE t.constraint_type='PRIMARY KEY'
+      AND t.table_schema=DATABASE()
+      AND t.table_name='{$table}';";
+
+    $this->exe = $this->link->query($query);
+    if ($this->exe) {
+
+      return $this->exe->fetch_assoc();
+
+    }
+
+  }
+
+  public function SelectAll($table, $columns = "*", $where = null, $join = null, $orderBY = null, $limit = null)
+  {
+    // SELECT * FROM `user`
+    // SELECT column_name FROM `user`
+    // SELECT column_name FROM `user` WHERE 
+    // SELECT column_name FROM `user` JOIN  WHERE 
+    // SELECT column_name FROM `user`   WHERE  ORDERBY
+    // SELECT column_name FROM `user`   WHERE  LIMIT 
+    $primaryKey = $this->getPrimaryKey($table);
+    // print_r($primaryKey);
+
+    $this->query = "SELECT {$columns} FROM `{$table}`";
+
+    if ($join != null) {
+      $this->query .= " JOIN {$join}";
+    }
+
+    if ($where != null) {
+      $this->query .= " WHERE {$where}";
+    }
+
+
+
+    if ($orderBY != null) {
+      $this->query .= " ORDER BY {$orderBY}";
+    } else {
+      // echo "dsaklkldsa";
+
+
+      $this->query .= " ORDER BY {$table}.{$primaryKey["COLUMN_NAME"]} DESC";
+    }
+
+    if ($limit != null) {
+      $this->query .= " LIMIT {$limit}";
+    }
+
+    if ($this->execute($this->query, null)) {
+      return true;
+
+    } else {
+      return false;
+    }
+
+  }
+
+
+  public function ShowResult($check = true, $col, $data)
+  {
+
+    if ($check) {
+
+      $html = "<div class='table-responsive'>";
+
+      $html .= "<table class='table table-dark table-hover'>";
+      // headings
+      $html .= "<tr>";
+      foreach ($col as $value) {
+        $html .= "<th>{$value} </th>";
+      }
+      $html .= "</tr>";
+      // heading end
+
+      // database fetching rows
+
+      while ($row = $this->GetRESULT()) {
+        $html .= "<tr>";
+
+        foreach ($data as $values) {
+          $html .= "<td>{$row[$values]}</td>";
+        }
+
+        $html .= "</tr>";
+
+      }
+
+
+      $html .= "
+    </table>
+    </div>";
+
+      return $html;
+
+    }else{
+    
+      $helper = new helper();
+
+      while ($abc=$this->GetRESULT()) {
+        $helper->pre($abc);
+      }
+    
+    }
+
+  }
+
+
+
+}
+
+
+class helper extends Database
+{
+
+  // private $obj=new Database();
+
+  public function pre($input)
+  {
+    if (is_array($input)) {
+      echo "<pre>";
+      print_r($input);
+      echo "</pre>";
+    } else {
+      echo $input;
+    }
+  }
+
+
+  public function redirectURL($url)
+  {
+
+    header("Location:" . $url);
+
+  }
+
+
+  public function RefreshURL($second = 2, $url)
+  {
+    // header("Refresh:2,url=home.php");
+
+    header("Refresh:" . $second . ",url=" . $url);
+  }
+
+  public function SuccessMSG($msg)
+  {
+    echo "<div class='alert alert-success' role='alert'>
+    {$msg}
+  </div>";
+
+  }
+  public function ErrorMSG($msg)
+  {
+    echo "<div class='alert alert-danger' role='alert'>
+     {$msg}
+   </div>";
+
+  }
+  public function FilterData($input)
+  {
+
+    $input = $this->link->real_escape_string($input);
+    $input = trim($input);
+    $input = stripslashes($input);
+
+    return $input;
+
+
+    // "\\hello\\"; // \hello\
+    // $input=mysqli_real_escape_string($link, $input);
+  }
+
+
+
+
+
+
+}
+
+?>
--- PHP OOPS/project/form.php
+++ PHP OOPS/project/form.php
@@ -1,19 +1,50 @@
-<?php 
-require_once(dirname(__FILE__) ."/include/database.php");
+<!doctype html>
+<html lang="en">
 
-use includes\database\Database as DB;
-use includes\database\helper as help;
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+<?php 
+require_once(dirname(__FILE__) ."/App/database.php");
+
+use App\database\Database as DB;
+use App\database\helper as help;
 
 $obj= new DB();
+
+$obj2= new DB();
+
 $helper= new help();
 
-if ($obj->SelectAll("users","*","id=2")) {
+// $obj2->SelectAll("users","users.*, user_task.*","user_task.user_id=5","user_task ON users.user_id = user_task.user_id","user_task.task_id DESC");
 
-while($row=$obj->GetRESULT()){
-$helper->pre($row);
-}
+// echo $obj2->getSql();
+// $helper->pre( $obj2->getPrimaryKey("user_task"));
+// LIMIT offset limit;
 
-}
+// $obj2->SelectAll("users","users.*,user_task.*",null,"user_task ON users.user_id = user_task.user_id",null,null);
+
+$obj2->SelectAll("users","*");
+ // $helper->pre($obj2->GetRESULT());
+$column=["#","userName","email"];//table heading 
+
+$values=["user_id","user_name","email"]; // database table field/column name
+
+ echo $obj2->ShowResult(true,$column, $values);
+
+
+
+
 
 $data=[
  "user_name"=>"XYZ",
@@ -29,3 +60,14 @@ $data=[
 // }
 
 ?>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
